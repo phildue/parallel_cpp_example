291274d8e3367ec084e209eeff94607907f6b857
phild <phild@protonmail.com>
2024-01-04 12:37:34 +0000

    cleanup

--- src/photometric_error.cpp
+++ src/photometric_error.cpp
@@ -5,9 +5,12 @@
 #include <execution>
 namespace exec = std::execution;
 #include <iostream>
+#include <map>
 #include <numeric>
+#include <opencv2/core/cuda.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+
 #include <ranges>
 namespace stdv = std::views;
 namespace stdr = std::ranges;
@@ -18,113 +21,78 @@ typedef Eigen::VectorXf VecXf;
 typedef Eigen::Vector2f Vec2f;
 typedef Eigen::Vector2i Vec2i;
 typedef Eigen::Vector3f Vec3f;
-typedef Eigen::Vector4f Vec4f;
-typedef Eigen::Matrix<double, 2, 2> Mat2f;
-typedef Eigen::Matrix<double, 3, 3> Mat3f;
 typedef Eigen::Matrix<double, 4, 4> Mat4f;
 
-namespace classic {
-template <typename Reproject>
-double compute(Reproject reproject, const cv::Mat& I0, const cv::Mat& I1); 
-}
-
-namespace sequential {
-template <typename Reproject>
-double compute(Reproject reproject, const cv::Mat& I0, const cv::Mat& I1); 
-}
-
-namespace parallel {
-template <typename Reproject>
-double compute(Reproject reproject, const cv::Mat& I0, const cv::Mat& I1); 
-}
-namespace par_two_step{
-template <typename Reproject>
-double compute(Reproject reproject, const cv::Mat& I0, const cv::Mat& I1); 
-}
 cv::Mat convertDepthMat(const cv::Mat& depth_, float factor);
 
-int main(int argc, char* argv[]) {
+static const std::vector<std::string> METHODS = {"parallel", "sequential", "par_two_step", "classic"};
 
+struct Args {
   float sx = 1.0;
   float sy = 1.0;
   int N = 100;
-  std::string method = "classic";
-  if (argc > 1) {
-    N = std::stoi(argv[1]);
-  }
-  if (argc > 2) {
-    sx = std::stof(argv[2]);
-    sy = std::stof(argv[2]);
-  }
-  if (argc > 3) {
-    method = std::string(argv[3]);
-  }
+  std::string method = "parallel";
+  static Args parse(int argc, char* argv[]);
+};
 
-  cv::Mat I0 = cv::imread(RESOURCE_DIR "/rgb0.png", cv::IMREAD_GRAYSCALE);
-  cv::Mat Z0 = convertDepthMat(cv::imread(RESOURCE_DIR "/depth0.png", cv::IMREAD_ANYDEPTH), 1.0 / 5000.0);
-  cv::Mat I1 = cv::imread(RESOURCE_DIR "/rgb1.png", cv::IMREAD_GRAYSCALE);
+std::function<double(const cv::Mat&, const cv::Mat&)> makeAlgorithm(const Args& args, const cv::Mat& Z0);
 
-  cv::resize(I0, I0, cv::Size{0, 0}, sx, sy);
-  cv::resize(Z0, Z0, cv::Size{0, 0}, sx, sy, cv::INTER_NEAREST);//No bilinear interpolation for depth
-  cv::resize(I1, I1, cv::Size{0, 0}, sx, sy);
+int main(int argc, char* argv[]) {
 
-  /*FIXME: the memory allocated by opencv is not using unified memory*/
-  std::vector<uint8_t> I0d(I0.begin<uint8_t>(), I0.end<uint8_t>());
-  std::vector<uint8_t> I1d(I1.begin<uint8_t>(), I1.end<uint8_t>());
-  std::vector<float> Z0d(Z0.begin<float>(), Z0.end<float>());
-  cv::Mat I0_{I0.rows, I0.cols, CV_8U, I0d.data()};
-  cv::Mat Z0_{I0.rows, I0.cols, CV_32F, Z0d.data()};
-  cv::Mat I1_{I0.rows, I0.cols, CV_8U, I1d.data()};
-  Mat4f pose = Mat4f::Identity();
+  const Args args = Args::parse(argc, argv);
 
-  // TUM-RGBD Dataset
-  auto reproject =
-    [fx = 525.0f*sx, fy = 525.0f*sy, cx = 319.5f*sx, cy = 239.5f*sy, w = (int)(640*sx), h = (int)(480*sy), Z0 = Z0d.data(), pose](const Vec2f& uv0) -> Vec2f {
-    const float z = Z0[(int)uv0(1) * w + (int)uv0(0)];
-    if (!std::isfinite(z) || z <= 0) {
-      return {-1, -1};
-    }
-    const Vec3f p0{(uv0(0) - cx) / fx * z, (uv0(1) - cy) / fy * z, z};
+  cv::Mat I0_ = cv::imread(RESOURCE_DIR "/rgb0.png", cv::IMREAD_GRAYSCALE);
+  cv::Mat Z0_ = convertDepthMat(cv::imread(RESOURCE_DIR "/depth0.png", cv::IMREAD_ANYDEPTH), 1.0 / 5000.0);
+  cv::Mat I1_ = cv::imread(RESOURCE_DIR "/rgb1.png", cv::IMREAD_GRAYSCALE);
 
-    /*Need to apply the transformation "manually" as otherwise cuda version does not compile due to "unsupported operation"*/
-    const Vec3f p0t = {
-      pose(0, 0) * p0(0) + pose(0, 1) * p0(1) + pose(0, 2) * p0(2) + pose(0, 3),
-      pose(1, 0) * p0(0) + pose(1, 1) * p0(1) + pose(1, 2) * p0(2) + pose(1, 3),
-      pose(2, 0) * p0(0) + pose(2, 1) * p0(1) + pose(2, 2) * p0(2) + pose(2, 3)};
+  // We have to allocate memory ourselves as otherwise its not "unified memory"
+  std::vector<uint8_t> i0v(I0_.rows * I0_.cols * args.sx * args.sy);
+  std::vector<uint8_t> i1v(I0_.rows * I0_.cols * args.sx * args.sy);
+  std::vector<float> z0v(I0_.rows * I0_.cols * args.sx * args.sy);
+  cv::Mat I0{I0_.rows * args.sy, I0_.cols * args.sx, CV_8U, i0v.data()};
+  cv::Mat Z0{I0_.rows * args.sy, I0_.cols * args.sx, CV_32F, z0v.data()};
+  cv::Mat I1{I0_.rows * args.sy, I0_.cols * args.sx, CV_8U, i1v.data()};
 
-    if (p0t(2) <= 0) {
-      return {-1, -1};
-    }
-    const Vec2f uv1{(fx * p0t(0) / p0t(2)) + cx, (fy * p0t(1) / p0t(2)) + cy};
-    return uv1;
-  };
-  std::function<double(const cv::Mat&, const cv::Mat&)> compute;
-  if (method == "parallel") {
-    compute = [reproject](auto a, auto b) { return parallel::compute(reproject, a, b); };
-  } else if (method == "sequential") {
-    compute = [reproject](auto a, auto b) { return sequential::compute(reproject, a, b); };
-  } else if (method == "par_two_step") {
-    compute = [reproject](auto a, auto b) { return par_two_step::compute(reproject, a, b); };
-  } else {
-    method = "classic";
-    compute = [reproject](auto a, auto b) { return classic::compute(reproject, a, b); };
-  }
+  cv::resize(I0_, I0, cv::Size{0, 0}, args.sx, args.sy);
+  cv::resize(Z0_, Z0, cv::Size{0, 0}, args.sx, args.sy, cv::INTER_NEAREST);  // No bilinear interpolation for depth
+  cv::resize(I1_, I1, cv::Size{0, 0}, args.sx, args.sy);
+
+  auto compute = makeAlgorithm(args, Z0);
+
   using timer = std::chrono::high_resolution_clock;
 
-  std::cout << "Running for [" << N << "] iterations on scale: [" << I0.cols << "," << I0.rows << "] with method: [" << method << "]"
-            << std::endl;
-  std::vector<double> dt(N);
+  std::cout << "Running for [" << args.N << "] iterations on scale: [" << I0.cols << "," << I0.rows << "] with method: [" << args.method
+            << "]" << std::endl;
+  std::vector<double> dt(args.N);
   double err = 0.;
-  for (int i = 0; i < N; i++) {
+  for (int i = 0; i < args.N; i++) {
     auto t0 = timer::now();
 
-    err = compute(I0_, I1_);
+    err = compute(I0, I1);
 
     auto t1 = timer::now();
     dt[i] = (t1 - t0).count() / 1e9;
   }
-  std::cout << "Mean = " << std::reduce(dt.begin(), dt.end()) / N << " Error: " << err << std::endl;
+  std::cout << "Mean = " << std::reduce(dt.begin(), dt.end()) / args.N << " Error: " << err << std::endl;
   return 0;
+}
+
+Args Args::parse(int argc, char* argv[]) {
+  Args args{};
+  if (argc > 1) {
+    args.N = std::stoi(argv[1]);
+  }
+  if (argc > 2) {
+    args.sx = std::stof(argv[2]);
+    args.sy = std::stof(argv[2]);
+  }
+  if (argc > 3) {
+    args.method = std::string(argv[3]);
+    if (!std::any_of(METHODS.begin(), METHODS.end(), [method = args.method](auto m) { return m == method; })) {
+      args.method = "parallel";
+    }
+  }
+  return args;
 }
 
 cv::Mat convertDepthMat(const cv::Mat& depth_, float factor) {
@@ -221,3 +189,39 @@ double compute(Reproject reproject, const cv::Mat& I0, const cv::Mat& I1) {
   return r / 255.f;
 }
 }  // namespace classic
+
+std::function<double(const cv::Mat&, const cv::Mat&)> makeAlgorithm(const Args& args, const cv::Mat& Z0) {
+  // TUM-RGBD Camera
+  auto reproject = [fx = 525.0f * args.sx,
+                    fy = 525.0f * args.sy,
+                    cx = 319.5f * args.sx,
+                    cy = 239.5f * args.sy,
+                    w = (int)(640 * args.sx),
+                    h = (int)(480 * args.sy),
+                    Z0 = (float*)Z0.data,
+                    pose = Mat4f::Identity()](const Vec2f& uv0) -> Vec2f {
+    const float z = Z0[(int)uv0(1) * w + (int)uv0(0)];
+    if (!std::isfinite(z) || z <= 0) {
+      return {-1, -1};
+    }
+    const Vec3f p0{(uv0(0) - cx) / fx * z, (uv0(1) - cy) / fy * z, z};
+
+    /*Need to apply the transformation "manually" as otherwise cuda version does not compile due to "unsupported operation"*/
+    const Vec3f p0t = {
+      pose(0, 0) * p0(0) + pose(0, 1) * p0(1) + pose(0, 2) * p0(2) + pose(0, 3),
+      pose(1, 0) * p0(0) + pose(1, 1) * p0(1) + pose(1, 2) * p0(2) + pose(1, 3),
+      pose(2, 0) * p0(0) + pose(2, 1) * p0(1) + pose(2, 2) * p0(2) + pose(2, 3)};
+
+    if (p0t(2) <= 0) {
+      return {-1, -1};
+    }
+    return {(fx * p0t(0) / p0t(2)) + cx, (fy * p0t(1) / p0t(2)) + cy};
+  };
+  const std::map<std::string, std::function<double(const cv::Mat&, const cv::Mat&)>> compute = {
+    {"parallel", [reproject](auto a, auto b) { return parallel::compute(reproject, a, b); }},
+    {"sequential", [reproject](auto a, auto b) { return sequential::compute(reproject, a, b); }},
+    {"par_two_step", [reproject](auto a, auto b) { return par_two_step::compute(reproject, a, b); }},
+    {"classic", [reproject](auto a, auto b) { return classic::compute(reproject, a, b); }},
+  };
+  return compute.at(args.method);
+}
